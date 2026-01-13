4be4dfe63c8da1a3ade4f6c27e906d855a8cee3e
yeseul jang <yyssjj9999@gmail.com>
2026-01-13 16:00:22 +0900

    🌟feat: Lv2 완료

--- YSBaseballGame/YSBaseballGame/main.swift
+++ YSBaseballGame/YSBaseballGame/main.swift
@@ -7,12 +7,59 @@
 
 import Foundation
 
+let computerNumbers = makeRandomNumbers()
+startGame()
+
 func makeRandomNumbers() -> [Int] {
     var randomNumbers = Set<Int>()
     
     while randomNumbers.count < 3 {
         randomNumbers.insert(Int.random(in: 1...9))
     }
-    
     return Array(randomNumbers)
+}
+
+func startGame() {
+    var isGameOn = true
+ 
+    while isGameOn {
+        print("✏️ 숫자를 입력하세요.")
+        guard let userInput = readLine(),
+              let cleanedNumbers = cleanNumbers(with: userInput) else {
+            print("❌ 잘못된 입력입니다. 겹치지 않는 세자리 숫자를 입력해주세요!")
+            continue
+        }
+        let (strikeCount, ballCount) = getGameResults(of: cleanedNumbers)
+        if strikeCount == 3 {
+            print("🎉 정답입니다!!")
+            isGameOn = false
+        } else {
+            print("\(strikeCount)스트라이크 \(ballCount)볼")
+        }
+    }
+}
+
+func getGameResults(of userNumbers: [Int]) -> (Int, Int) {
+    let sameNumbers = computerNumbers.filter { userNumbers.contains($0) }
+    var strikeCount = 0
+    var ballCount = 0
+    
+    for num in sameNumbers {
+        if computerNumbers.firstIndex(of: num) == userNumbers.firstIndex(of: num) {
+            strikeCount += 1
+        }
+    }
+    ballCount = sameNumbers.count - strikeCount
+    
+    return (strikeCount, ballCount)
+}
+
+func cleanNumbers(with input:String) -> [Int]? {
+    let cleanedNumbers = input.split(separator: "").compactMap { Int($0) }.filter { $0 != 0 }
+    
+    if cleanedNumbers.count > 3 || Set(cleanedNumbers).count != 3 {
+        return nil
+    } else {
+        return cleanedNumbers
+    }
 }
