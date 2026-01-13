46ba4c694801fac94cc66f63a71fac712d8c6d57
yeseul jang <yyssjj9999@gmail.com>
2026-01-13 17:40:02 +0900

    🌟feat: Lv4 showMenu 메서드 추가, makeRandomNumberWithZero() 수정

--- YSBaseballGame/YSBaseballGame/main.swift
+++ YSBaseballGame/YSBaseballGame/main.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-let computerNumbers = makeRandomNumberWithZero()
-startGame()
+var computerNumbers = makeRandomNumberWithZero()
+showMenu()
 
 func makeRandomNumbers() -> [Int] {
     var randomNumbers = Set<Int>()
@@ -26,16 +26,47 @@ func makeRandomNumberWithZero() -> [Int] {
         randomNumbers.insert(Int.random(in: 0...9))
     }
     var resultNumbers = Array(randomNumbers)
-    resultNumbers.insert(Int.random(in: 1...9), at: 0)
-
+    
+    while resultNumbers.count < 3 {
+        let firstNum = Int.random(in: 1...9)
+        if !(resultNumbers.contains(firstNum)) {
+            resultNumbers.insert(firstNum, at: 0)
+        }
+    }
+    print(resultNumbers)
     return resultNumbers
+}
+
+func showMenu() {
+    let startOption = "1"
+    let gameRecordOption = "2"
+    let endOption = "3"
+    var isMenuShowing = true
+    
+    while isMenuShowing {
+        print("1. 게임시작 2. 게임기록보기 3. 게임종료\n원하는 기능을 선택해주세요.")
+        guard let menuChoice = readLine() else { return }
+        
+        switch menuChoice {
+        case startOption:
+            startGame()
+        case gameRecordOption:
+            print("기록 기능 준비중")
+        case endOption:
+            isMenuShowing = false
+        default:
+            print("다시 입력해주세요.")
+            continue
+        }
+    }
 }
 
 func startGame() {
     var isGameOn = true
  
     while isGameOn {
-        print("✏️ 숫자를 입력하세요.")
+        print("⚾️게임을 시작합니다!⚾️\n✏️ 숫자를 입력하세요.")
+        
         guard let userInput = readLine(),
               let cleanedNumbers = cleanNumbers(with: userInput) else {
             print("❌ 잘못된 입력입니다. 겹치지 않는 세자리 숫자를 입력해주세요!")
@@ -45,12 +76,13 @@ func startGame() {
         let (strikeCount, ballCount) = getGameResults(of: cleanedNumbers)
         
         if strikeCount == 3 {
-            print("🎉 정답입니다!!")
+            print("🎉🎉🎉🎉🎉\n🎉 정답입니다!!🎉\n🎉🎉🎉🎉🎉")
             isGameOn = false
         } else {
             print("\(strikeCount)스트라이크 \(ballCount)볼")
         }
     }
+    computerNumbers = makeRandomNumberWithZero()
 }
 
 func getGameResults(of userNumbers: [Int]) -> (Int, Int) {
